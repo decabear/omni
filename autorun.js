4ec78019d3da0797a855cb0bd6ef31a72715eff5
keegan <keegan@color.com>
2012-03-03 21:50:55 -0800

    forgot what i did, but i'm committing regardless

--- autorun.js
+++ autorun.js
@@ -1,15 +1,14 @@
 $(function(){
-  var port = chrome.extension.connect({name: 'asdf'});
+  var port = chrome.extension.connect();
 
   port.onMessage.addListener(function(msg){
     console.log('context:', msg)
   })
 
-  console.log('getting current tab');
-  chrome.tabs.getCurrent(function(tab) {
-    console.log('current tab', tab, arguments);
-    port.postMessage({type: 'get-context', tabId: tab.id})
+  port.postMessage({
+    innerText: document.body.innerText,
+    title: document.title,
+    url: location.href
   });
-
 });
 
