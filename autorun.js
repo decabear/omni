991a1a2310487402b41584269b22b23d66182501
keegan <keegan@color.com>
2012-03-03 18:07:05 -0800

    switched stuff around toward the new goal, set up the new tab page

--- autorun.js
+++ autorun.js
@@ -1,21 +1,10 @@
 $(function(){
-  //alert('sup brah')
-  
-  console.log('this works')
-
-  //console.log(chrome.browserAction)
-  //chrome.browserAction.setBadgeText("keegan")
-
-  console.log('asdasdf')
-
   var port = chrome.extension.connect({name: 'asdf'});
 
   port.onMessage.addListener(function(msg){
-    console.log('cli msg', msg)
+    console.log('msg from backend:', msg)
   })
 
   port.postMessage('hi')
-
-  console.log(port)
-})
+});
 
